850d720d412a6a6b5b394d72be201f702eef1817
marek2222 <marekmusial2@o2.pl>
2019-05-04 23:30:32 +0200

    API 3.27: Creating the Login repository method

--- DatingApp/DatingApp.API/Data/AuthRepository.cs
+++ DatingApp/DatingApp.API/Data/AuthRepository.cs
@@ -12,9 +12,30 @@ namespace DatingApp.API.Data
     {
       _context = context;
     }
-    public Task<User> Login(string username, string password)
+    public async Task<User> Login(string username, string password)
     {
-      throw new System.NotImplementedException();
+      var user = await _context.Users.FirstOrDefaultAsync(x => x.Username === username);
+
+      if (user == null)
+        return null;
+
+      if (!VerifyPasswordHash(password, user.passwordHash, user.passwordSalt))
+        return null;
+
+        return user;
+    }
+
+    private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+    {
+      using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordHash))
+      {
+        var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+          if (computedHash[i] != passwordHash[i]) return false;
+        }
+      }
+      return true;
     }
 
     public async Task<User> Register(User user, string password)
@@ -31,22 +52,22 @@ namespace DatingApp.API.Data
       return user;
     }
 
-        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
     {
-       using (var hmac = new System.Security.Cryptography.HMACSHA512())
-       {
-           passwordSalt = hmac.Key;
-           passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+      using (var hmac = new System.Security.Cryptography.HMACSHA512())
+      {
+        passwordSalt = hmac.Key;
+        passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
 
-       }
+      }
     }
 
-        public async Task<bool> UserExists(string username)
+    public async Task<bool> UserExists(string username)
     {
-            if (await _context.Users.AnyAsync(x => x.Username == username))
-                return true;
+      if (await _context.Users.AnyAsync(x => x.Username == username))
+        return true;
 
-            return false;
+      return false;
     }
   }
 }
